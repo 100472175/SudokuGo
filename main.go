45761b024780c5639a448872a56b7a667f0cf523
edu <100472175@alumnos.uc3m.es>
2024-07-11 11:24:03 +0200

    refrased error messages

--- main.go
+++ main.go
@@ -13,8 +13,8 @@ import (
 )
 
 func main() {
-	if len(os.Args) < 2 || len(os.Args) > 3 {
-		fmt.Println("Usage: go run main.go <input_file> [output_file]")
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: go run main.go <input_file>")
 		os.Exit(0)
 	}
 	now := time.Now()
@@ -41,7 +41,7 @@ func main() {
 			fmt.Println("The 16x16 Sudoku has been solved. This is the result ->")
 			large.PrintBoard(board)
 		} else {
-			fmt.Println("The 16x16 Sudoku can't be solved.")
+			fmt.Println("The 16x16 Sudoku couldn't be solved.")
 		}
 	case 9:
 		board := medium.ParseInput(os.Args[1])
@@ -50,7 +50,7 @@ func main() {
 			fmt.Println("The 9x9 Sudoku has been solved. This is the result ->")
 			medium.PrintBoard(board)
 		} else {
-			fmt.Printf("The 9x9 Sudoku can't be solved.")
+			fmt.Printf("The 9x9 Sudoku couldn't be solved.")
 		}
 	case 4:
 		board := small.ParseInput(os.Args[1])
@@ -58,7 +58,7 @@ func main() {
 			fmt.Println("The 4x4 Sudoku has been solved. This is the result ->")
 			small.PrintBoard(board)
 		} else {
-			fmt.Println("The 4x4 Sudoku can't be solved.")
+			fmt.Println("The 4x4 Sudoku couldn't be solved.")
 		}
 	default:
 		fmt.Println("Invalid number of columns. Please enter 4, 9, or 16 in the file.")
